af926691535062e13fca976fe1e06c02ae52e54c
vale1202 <44300221+vale1202@users.noreply.github.com>
2018-10-24 13:21:44 -0500

    Update responder.groovy

--- responder/responder.groovy
+++ responder/responder.groovy
@@ -1,7 +1,7 @@
 def hello(greet){ //defining a function called "hello" which receives an argument and stores it in the "greet" variable.
 if (greet == "hi"){ //if the greet value iquals "hello" then executes the println "Hello from responder file!" 
 	println 'Hello from responder file!' 
-	println 'Hello $NAME'
+	println "Hello $NAME"
 } else { // if the greet values is diffrent to "hello" then executes the println "wzup"
 	println 'wzup'  
  } 
